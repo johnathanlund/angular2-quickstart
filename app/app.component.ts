85634b2d06dbdb2ecfcf7bd41c0e4937647fe866
johnathanlund <jalshield@gmail.com>
2016-10-02 12:09:59 -0600

    Finished Angular's quickstart guide. About to start their next tutorial Tour of Heroes.

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>My First Angular2 App</h1> <h2>Live Change</h2> <h1>Playing around now</h1>'
+})
+export class AppComponent {}
