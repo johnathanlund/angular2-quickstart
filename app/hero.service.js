f88c797c28b1ed5798f61dcd60097adf099cdd19
johnathanlund <jalshield@gmail.com>
2016-10-06 23:22:58 -0600

    Completed part 5 of Tour of Heroes tutorial, routing

--- app/hero.service.js
+++ app/hero.service.js
@@ -8,8 +8,8 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require('@angular/core');
 var mock_heroes_1 = require('./mock-heroes');
+var core_1 = require('@angular/core');
 var HeroService = (function () {
     function HeroService() {
     }
@@ -22,6 +22,10 @@ var HeroService = (function () {
             return setTimeout(resolve, 2000);
         }) // delay 2 seconds
             .then(function () { return _this.getHeroes(); });
+    };
+    HeroService.prototype.getHero = function (id) {
+        return this.getHeroes()
+            .then(function (heroes) { return heroes.find(function (hero) { return hero.id === id; }); });
     };
     HeroService = __decorate([
         core_1.Injectable(), 
